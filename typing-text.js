f0fd74c11d82239caf303dd6bb9fb08fdab8bdaf
Shelby <shelbyyang.23@gmail.com>
2023-07-25 14:50:47 -0500

    typing-text script

---
+++ typing-text.js
@@ -0,0 +1,40 @@
+let i = 0;
+let j = 0;
+
+// Phrases to be displayed
+let phrases = [
+    "Hey!",
+    "I'm Shelby Yang.",
+    "I've used <span style='color: red;'>Java</span>, <span style='color: blue;'>C++</span>, <span style='color: yellow;'>Python</span>, <span style='color: green;'>SQL</span>, and more.",
+    "Please use my website to learn more about me."
+];
+
+window.onload = loop;
+
+function loop() {
+    let element = document.querySelector('#typing-text');
+    let speed = 75; // Typing speed
+
+    // Get the current phrase
+    let currentPhrase = phrases[j];
+
+    // Typing Effect
+    if (i < currentPhrase.length) {
+        // Add a character
+        element.innerHTML += currentPhrase.charAt(i);
+        i++;
+    }
+
+    // Move to the next phrase
+    if (i === currentPhrase.length) {
+        i = 0;
+        j++;
+        if(j < phrases.length) {
+            element.innerHTML += '<br>'; // Add a line break after each phrase
+        } else {
+            return; // End the function after all phrases have been typed
+        }
+    }
+
+    setTimeout(loop, speed);
+}
